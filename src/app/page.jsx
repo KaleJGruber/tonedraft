08b673e858b0495b89af8908f7153507081fa623
KaleJGruber <kalegruber1@gmail.com>
2026-04-26 02:33:20 -0500

    Change import to named import for FreeMessageCounter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import FreeMessageCounter from "./components/FreeMessageCounter";
+import { FreeMessageCounter } from "./components/FreeMessageCounter";
+
 
 
 export default function Home() {
