9698367ec42e085cd9510c45d0685fcf1dd82644
Kale Gruber <kalegruber@Kales-MacBook-Air.local>
2026-05-13 14:55:27 -0500

    fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,9 +2,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-export function createBrowserClient() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
